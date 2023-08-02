0658ae3289541dc71c1c3086c2205e4b24c09ab5
Ruslan Rasulov <ruslan@macOS-Ruslan-2.local>
2023-08-02 14:37:05 +0300

    потестил хаски

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+const { propertyOrdering, selectorOrdering } = require("stylelint-semantic-groups");
+
+module.exports = {
+  plugins: ["stylelint-order"],
+  rules: {
+    "order/order": selectorOrdering,
+    "order/properties-order": propertyOrdering,
+  },
+};
