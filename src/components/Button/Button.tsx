d6ba8467810054f08be8f1c94407eee27259e1be
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-08-04 23:04:39 +0600

    fix: линтер

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,33 +1,21 @@
+import classnames from "classnames"
 import React, { FC } from "react"
-import classnames from 'classnames'
 
-import styles from './Button.module.css'
+import styles from "./Button.module.css"
 
 interface Props {
   title: string
-  type: 'button' | 'submit'
+  type: "button" | "submit"
   onClick: () => void
   disabled: boolean
   className?: string
-
 }
 
-export const Button: FC<Props> = ({
-  title,
-  type,
-  onClick,
-  disabled,
-  className,
-}) => {
+export const Button: FC<Props> = ({ title, type, onClick, disabled, className }) => {
   const finalClassName = classnames(styles.button, className)
 
   return (
-    <button
-      type={type}
-      onClick={onClick}
-      disabled={disabled}
-      className={finalClassName}
-    >
+    <button type={type} onClick={onClick} disabled={disabled} className={finalClassName}>
       {title}
     </button>
   )
