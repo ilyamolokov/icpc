b14d4c9b218ee51910263468a67bb551bbd487a0
Ilya Molokov <lumberjack.im@gmail.com>
2023-08-05 15:53:44 +0600

    feat: добавил компонент с условием задачи

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 
-import { Comments } from "./Comments/Comments"
+import { TaskDescription } from "./TaskDescription/TaskDescription"
 
 const App = () => {
   return (
     <div>
-      <Comments />
+      <TaskDescription />
     </div>
   )
 }
