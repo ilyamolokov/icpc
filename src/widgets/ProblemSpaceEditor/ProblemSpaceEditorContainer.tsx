46e130c834b8ca2c719824dabfe7cbe3331e0b32
Ruslan <ruslan@macOS-Ruslan-2.local>
2023-08-11 15:29:21 +0300

    chor: исправил postSubmissions и прикрутил на время поулчение апи вердиктов

--- src/widgets/ProblemSpaceEditor/ProblemSpaceEditorContainer.tsx
+++ src/widgets/ProblemSpaceEditor/ProblemSpaceEditorContainer.tsx
@@ -2,9 +2,10 @@ import React, { FC, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 import { api } from "../../api"
+import { getRunId } from "../../helpers/getRunId"
 import { CodeHandler, socket } from "../../sockets"
-import { ProblemSpaceEditor } from "./ProblemSpaceEditor"
 import { useGetYandexUserQuery } from "../../store/api/user.api"
+import { ProblemSpaceEditor } from "./ProblemSpaceEditor"
 
 export const ProblemSpaceEditorContainer: FC = () => {
   const { alias } = useParams()
@@ -13,18 +14,34 @@ export const ProblemSpaceEditorContainer: FC = () => {
 
   const { data: user } = useGetYandexUserQuery()
 
-  const training_session_id = "c9b5c66e-e1d8-4579-9ab9-4fd2adc4b6db"
-  const sendCode = (code: string) => {
-    const trainingSessionId = "c9b5c66e-e1d8-4579-9ab9-4fd2adc4b6db"
-    api.postSubmissions(trainingSessionId, code, "python3_docker", "A").then(console.log).catch(console.log)
+  const trainingSessionId = "c9b5c66e-e1d8-4579-9ab9-4fd2adc4b6db"
+
+  const fetchSubmissionDetails = async (runId: number) => {
+    try {
+      const res = await api.getSubmissionsFull(trainingSessionId, runId)
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const sendCode = async (code: string) => {
+    try {
+      const res = await api.postSubmissions(trainingSessionId, code, "python3_docker", "A")
+      const runId = getRunId(res.data)
+      console.log(runId)
+      fetchSubmissionDetails(runId) // вызываем функцию после получения runId
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   const onCodeChange = (code: string) => {
-    socket.sendCode({ code, problemAlias: alias, userId: user.id})
+    socket.sendCode({ code, problemAlias: alias, userId: user.id })
   }
 
   const editorEventHandler: CodeHandler = ({ code, userId, problemAlias }) => {
-    if(userId !== user.id && problemAlias === alias) {
+    if (userId !== user.id && problemAlias === alias) {
       setCodeState(code)
     }
   }
@@ -33,7 +50,7 @@ export const ProblemSpaceEditorContainer: FC = () => {
     socket.subscribeEditor(editorEventHandler)
 
     api
-      .getCodeByProblemAlias(training_session_id, alias)
+      .getCodeByProblemAlias(trainingSessionId, alias)
       .then(({ code }) => setCodeState(code))
       .catch(console.log)
   }, [alias])
